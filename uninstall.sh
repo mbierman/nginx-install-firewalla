e25aa360f901f064d31700998b9edb9018a7d003
Michael Bierman <mbierman@users.noreply.github.com>
2023-11-08 03:40:20 -0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,6 @@
 
 sudo systemctl stop docker
 # sudo rm -rf  /data/nginix /data/nginx /data/ngnix 
-sudo docker stop nginxproxymanager_app_1 nginxproxymanager_db_1 && sudo docker system prune -a  && sudo docker image prune -a && sudo docker container prune -a 
+sudo docker stop 805f32b12b72 && sudo docker stop nginxproxymanager_app_1 nginxproxymanager_db_1 && sudo docker system prune -a  && sudo docker image prune -a && sudo docker container prune -a 
 sudo rm -rf  /data/nginix /data/nginx /data/ngnix
 sudo docker system prune -a 
